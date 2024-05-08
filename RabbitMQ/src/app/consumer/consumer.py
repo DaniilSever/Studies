a933af38029b0378c0dbbaa6e494642cc6d59a8e
DaniilSever <jhjsjfiohig14@mail.ru>
2024-05-09 01:21:20 +0700

    finaly fixited

--- RabbitMQ/src/app/consumer/consumer.py
+++ RabbitMQ/src/app/consumer/consumer.py
@@ -6,27 +6,24 @@ from pika import (
     URLParameters,
 )
 
-url_rmq_params = URLParameters('amqp://rmuser:rmpassword@215.21.0.54:15672/%2F')
-url_params = URLParameters(os.environ['AMQP_URL'])
 credentials = PlainCredentials('rmuser', 'rmpassword')
 rmq_params = ConnectionParameters(
     host="215.21.0.54",
-    port=15672,
+    port=5672,
     credentials=credentials,
     heartbeat=30
 )
 
 def main():
-    connection = URLParameters(url_params)
+    connection = BlockingConnection(rmq_params)
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello', durable=True)
+    channel.queue_declare(queue='hello')
 
     def callback(ch, method, properties, body):
         print(" [x] Received %r" % body)
 
-    channel.basic_consume(callback,
-                          queue='hello',
+    channel.basic_consume(queue='hello',
                           on_message_callback=callback,
                           auto_ack=True)
     
--- RabbitMQ/src/app/publisher/publisher.py
+++ RabbitMQ/src/app/publisher/publisher.py
@@ -6,20 +6,18 @@ from pika import (
     URLParameters,
 )
 
-url_rmq_params = URLParameters('amqp://rmuser:rmpassword@215.21.0.54:15672/%2F')
-url_params = URLParameters(os.environ['AMQP_URL'])
 credentials = PlainCredentials('rmuser', 'rmpassword')
 rmq_params = ConnectionParameters(
     host="215.21.0.54",
-    port=15672,
+    port=5672,
     credentials=credentials,
     heartbeat=30
 )
 def first_send():
-    connection = BlockingConnection(url_params)
+    connection = BlockingConnection(rmq_params)
     channel = connection.channel()
 
-    channel.queue_declare(queue='hello', durable=True)
+    channel.queue_declare(queue='hello')
 
     channel.basic_publish(exchange='',
                         routing_key='hello',
