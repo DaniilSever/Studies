7c14ab0e2a8f52138f61ffd6f9b06bd9fe383b03
Daniil <jhisjfiohig14@mail.ru>
2024-06-10 22:51:15 +0700

    complete 'publish_subscribe'

--- Nats.io/nats_core/publish_subscribe/consumer/src/app/main.py
+++ Nats.io/nats_core/publish_subscribe/consumer/src/app/main.py
@@ -1,30 +1,10 @@
 import asyncio
-import nats
-from nats.errors import TimeoutError
+from threading import Thread
+from src import consumers
 
-async def consumer():
-    nc = await nats.connect("nats://demo.nats.io:4222")
+try:
+    for consumer in consumers:
+        Thread(target=asyncio.run(consumer)).start()
 
-    async def message_handler(msg):
-        subject = msg.subject
-        reply = msg.reply
-        data = msg.data.decode()
-        print("Received a message on '{subject} {reply}': {data}".format(
-            subject=subject, reply=reply, data=data))
-
-    sub = await nc.subscribe("foo", cb=message_handler)
-    await sub.unsubscribe(limit=2)
-    sub = await nc.subscribe("bar")
-    
-    try:
-        async for msg in sub.messages:
-            print(f"Received a message on '{msg.subject} {msg.reply}': {msg.data.decode()}")
-            await sub.unsubscribe()
-    except Exception as e:
-        pass
-
-    await nc.drain()
-
-
-if __name__ == '__main__':
-    asyncio.run(consumer())
+except Exception as e:
+    print(str(e))
--- Nats.io/nats_core/publish_subscribe/publisher/src/app/main.py
+++ Nats.io/nats_core/publish_subscribe/publisher/src/app/main.py
@@ -2,34 +2,18 @@ import asyncio
 import nats
 from nats.errors import TimeoutError
 
+
 async def publisher():
 
     nc = await nats.connect("nats://demo.nats.io:4222")
+    queue_name = "fanout_nats"
 
-    await nc.publish("foo", b'Hello')
-    await nc.publish("foo", b'World')
-    await nc.publish("foo", b'!!!!!')
-
-    await nc.publish("bar", b'First')
-    await nc.publish("bar", b'Second')
-
-
-    async def help_request(msg):
-        print(f"Received a message on '{msg.subject} {msg.reply}': {msg.data.decode()}")
-        await nc.publish(msg.reply, b'I can help')
-
-    sub = await nc.subscribe("help", "workers", help_request)
-    
-    try:
-        response = await nc.request("help", b'help me', timeout=0.5)
-        print("Received response: {message}".format(
-            message=response.data.decode()))
-    except TimeoutError:
-        print("Request timed out")
-
-    await sub.unsubscribe()
+    for _ in range(0, 10):
+        await nc.publish(queue_name, b"Test_message")
+        await asyncio.sleep(0.5)
 
     await nc.drain()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(publisher())
